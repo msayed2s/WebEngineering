4df82254ee77c902c3aad8cbb5f5049888aa3b79
msayed2s <moe.said@hotmail.com>
2020-12-31 04:47:58 +0100

    added fancy styles and a button to switch between them

--- javascript.js
+++ javascript.js
@@ -1,6 +1,8 @@
 var counter = 0;
 var bool = [];
 
+var changedBackground = true;
+
 function generator(title, pfad, nummer ) {
     var list = document.getElementById("div" + nummer);
     main = document.getElementById("main")
@@ -47,73 +49,6 @@ function test(nummer) {
             counter = 0;
             bool[nummer] = 99;
             break;
-
-            //  var list = document.getElementById("div" + nummer);
-            //  main = document.getElementById("main")
-            //  var domasIdee = ["CSS Positionierung Teil 1", "CSS Positionierung Teil 2", "Wireframe with HTML and CSS"]
-            //  var domasIdee2 = ["zweiPunktEins.html", "zweiPunktZwei.html", "zweiPunktDrei.html"]
-
-            //     for(let i = 0; i < domasIdee.length; i++) {
-            //         var newItem1 = document.createElement("a");
-            //         var textnode = document.createTextNode(domasIdee[i]);
-        
-            //         newItem1.setAttribute('href', "")
-            //         newItem1.addEventListener('click', event => {
-            //             event.preventDefault();
-            //             const url = 'Uebung2/' + domasIdee2[i]
-            //             main.innerHTML = '<iframe style="width:100%;height:100%;" frameborder="0" src="' + url + '" />';
-
-        
-            //          })
-                    
-            //         newItem1.setAttribute('id', 'test1')
-
-            //         newItem1.appendChild(textnode);
-            //         list.appendChild(newItem1)
-            //         list.appendChild(document.createElement("br"))
-
-            //      }
-             
-
-            // console.log(bool[nummer])
-            // var newItem1 = document.createElement("a");
-            // var textnode = document.createTextNode("CSS Positionierung Teil 1");
-
-            // newItem1.setAttribute('href', "")
-            // newItem1.addEventListener('click', event => {
-            //     event.preventDefault();
-            //     const url = 'Uebung2/zweiPunktEins.html'
-            //     main.innerHTML = '<iframe style="width:100%;height:100%;" frameborder="0" src="' + url + '" />';
-            //     console.log( 'Keine Weiterleitung, sondern Ausgabe auf console' );
-
-
-            //     var dom = myFunction("Uebung2/zweiPunktZwei.html")
-
-            //  })
-            // // newItem1.setAttribute('href', "Uebung2/zweiPunktEins.html")
-            // newItem1.setAttribute('id', 'test1')
-            // // newItem1.setAttribute('target', '_blank')
-            // newItem1.appendChild(textnode);
-
-            // var newItem2 = document.createElement("a");
-            // var textnode = document.createTextNode("CSS Positionierung Teil 2");
-            // newItem2.setAttribute('href', "Uebung2/zweiPunktZwei.html")
-            // newItem2.appendChild(textnode);
-
-            // var newItem3 = document.createElement("a");
-            // var textnode = document.createTextNode("Wireframe with HTML and CSS");
-            // newItem3.setAttribute('href', "Uebung2/zweiPunktDrei.html")
-            // newItem3.appendChild(textnode);
-
-            // var breaker = document.createElement("br");
-
-            // var list = document.getElementById("div" + nummer);
-            // list.appendChild(newItem1);
-            // list.appendChild(document.createElement("br"))
-            // list.appendChild(newItem2);
-            // list.appendChild(document.createElement("br"))
-            // list.appendChild(newItem3);
-
         
 
         case 2:
@@ -212,29 +147,32 @@ function test(nummer) {
 }
 
 
-function httpGet(theUrl)
-{
-    var xmlHttp = new XMLHttpRequest();
-    xmlHttp.open( "GET", theUrl, false ); // false for synchronous request
-    xmlHttp.send( null );
-    return xmlHttp.responseText;
+
+
+
+// var tmpp = document.getElementById('test1').addEventListener("onclick", event => {
+//     event.preventDefault();
+//     console.log("Doma ist Baba");
+// })
+
+
+function changeStyle() {
+
+
+    if(changedBackground) {
+        document.head.innerHTML  = document.head.innerHTML.replace("blue.css", "white.css");
+        changedBackground = !changedBackground;
+
+    } else {
+        document.head.innerHTML = document.head.innerHTML.replace("white.css", "blue.css");
+        changedBackground = !changedBackground;
+
+    }
+    
+
+    
+    console.log("Hi")
 }
-
-
-
-function myFunction(url) {
-	Event.preventDefault()
-    var n = window.open(url);
-    return n;
-  
-}
-
-
-var tmpp = document.getElementById('test1').addEventListener("onclick", event => {
-    event.preventDefault();
-    console.log("Doma ist Baba");
-})
-
 
 // document.addEventListener(onclick, event => {
 //     var tmpp = document.getElementById('main')
